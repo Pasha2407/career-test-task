264c990f7174ba9aae7a4aebbefb154bb0abd7ae
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2024-03-10 02:54:06 +0200

    add favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+
 import css from './Favorites.module.css';
+import image from 'data/favorite.jpg';
+
+import { selectFavorite } from '../../redux/favorite/favoriteSelectors';
+import { CarList } from 'components/CarList/CarList';
+import { getFilteredCars } from 'helpers/getFilteredCars';
 
 export const Favorites = () => {
-  return <div className={css.Container}>Favorites</div>;
+  const favorite = useSelector(selectFavorite);
+  const filteredCars = getFilteredCars(favorite);
+
+  return (
+    <div className={css.Container}>
+      {favorite.length === 0 ? (
+        <div className={css.Image} style={{ backgroundImage: `url(${image})` }}>
+          <h1>You don't have any favorite cars yet</h1>
+        </div>
+      ) : (
+        <CarList data={filteredCars} />
+      )}
+    </div>
+  );
 };
